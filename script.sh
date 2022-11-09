7f98eb2e87b9fdd73f8c93bf6d05f22cf2d356bd
wilson jaramillo <wilsonjara13@gmail.com>
2022-11-09 21:32:31 +0100

    feature/menu menu hecho

--- script.sh
+++ script.sh
@@ -1,7 +1,29 @@
 #TODO: Menú del script
+while :
+do
+
+#menu
 clear
 echo "MENU SCRIPT v.0.1"
 echo "================="
 echo "1. Mostrar contenido directorio"
 echo "2. Salir"
 
+#Escoger menu
+echo -n "Escoger opcion:"
+read opcion
+
+#Seleccion de menu
+case $opcion in
+1) echo "Mostrando directorio"
+echo ""
+ls
+read foo
+;;
+2)exit 0;;
+
+#alerta 
+*) echo "Opcion invalida :("
+sleep 1
+esac
+done
